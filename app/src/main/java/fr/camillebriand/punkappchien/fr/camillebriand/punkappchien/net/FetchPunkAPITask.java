91dfb685c568e731c33eb454fdcad07e0c6369c1
Camille Briand <camille.briand@efrei.net>
2020-02-29 15:54:54 +0100

    Add public constructor to FetchPunkAPITask class

--- app/src/main/java/fr/camillebriand/punkappchien/fr/camillebriand/punkappchien/net/FetchPunkAPITask.java
+++ app/src/main/java/fr/camillebriand/punkappchien/fr/camillebriand/punkappchien/net/FetchPunkAPITask.java
@@ -1,11 +1,24 @@
 package fr.camillebriand.punkappchien.fr.camillebriand.punkappchien.net;
 
+import android.app.Activity;
 import android.os.AsyncTask;
+
+import java.lang.ref.WeakReference;
 
 public class FetchPunkAPITask extends AsyncTask {
 	private static final String BASE_API_PATH = "https://api.punkapi.com/v2/beers";
 	
 	private static final String RANDOM_BEER_API_PATH = BASE_API_PATH + "/random";
+	
+	private WeakReference<Activity> activityRef;
+	
+	/**
+	 * Create a new task to fetch the PunkAPI
+	 * @param activity Activity to reference
+	 */
+	public FetchPunkAPITask(Activity activity) {
+		this.activityRef = new WeakReference<>(activity);
+	}
 	
 	@Override
 	protected Object doInBackground(Object[] objects) {
