ad436fec7acaeaf55588d39457e6e02084d87c71
Camille Briand <camille.briand@efrei.net>
2020-02-29 15:52:40 +0100

    Add private static fields to reference PunkAPI in FetchPunkAPITask class

--- app/src/main/java/fr/camillebriand/punkappchien/fr/camillebriand/punkappchien/net/FetchPunkAPITask.java
+++ app/src/main/java/fr/camillebriand/punkappchien/fr/camillebriand/punkappchien/net/FetchPunkAPITask.java
@@ -3,6 +3,10 @@ package fr.camillebriand.punkappchien.fr.camillebriand.punkappchien.net;
 import android.os.AsyncTask;
 
 public class FetchPunkAPITask extends AsyncTask {
+	private static final String BASE_API_PATH = "https://api.punkapi.com/v2/beers";
+	
+	private static final String RANDOM_BEER_API_PATH = BASE_API_PATH + "/random";
+	
 	@Override
 	protected Object doInBackground(Object[] objects) {
 		return null;
