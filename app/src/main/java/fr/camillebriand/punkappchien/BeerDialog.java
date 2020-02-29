a599709342167303c5a3a959094dc4a5198d3dbe
Camille Briand <camille.briand@efrei.net>
2020-02-29 20:16:47 +0100

    Add a spinner to Beer dialog

--- app/src/main/java/fr/camillebriand/punkappchien/BeerDialog.java
+++ app/src/main/java/fr/camillebriand/punkappchien/BeerDialog.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class BeerDialog extends DialogFragment implements View.OnClickListener {
@@ -16,6 +18,7 @@ public class BeerDialog extends DialogFragment implements View.OnClickListener {
 	private TextView beerName;
 	private ImageView beerImage;
 	private TextView beerDescription;
+	private ProgressBar spinner;
 	
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class BeerDialog extends DialogFragment implements View.OnClickListener {
 		this.beerName = dialogView.findViewById(R.id.beer_dialog__name);
 		this.beerImage = dialogView.findViewById(R.id.beer_dialog__image);
 		this.beerDescription = dialogView.findViewById(R.id.beer_dialog__description);
+		this.spinner = dialogView.findViewById(R.id.beer_dialog__spinner);
 		
 		dialogView.findViewById(R.id.beer_dialog_dismiss_button).setOnClickListener(this);
 		
@@ -43,6 +47,7 @@ public class BeerDialog extends DialogFragment implements View.OnClickListener {
 	public void setBeerImage(Bitmap bmp) {
 		if (this.beerImage == null) return;
 		
+		((ViewGroup) this.spinner.getParent()).removeView(this.spinner);
 		this.beerImage.setImageBitmap(bmp);
 	}
 	
