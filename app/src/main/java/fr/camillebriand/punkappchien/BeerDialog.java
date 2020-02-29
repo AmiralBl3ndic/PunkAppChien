120bc72faa08f13d3f4fd9240943064f5c5f8d13
Camille Briand <camille.briand@efrei.net>
2020-02-29 19:51:09 +0100

    Add methods to change widgets in BeerDialog class

--- app/src/main/java/fr/camillebriand/punkappchien/BeerDialog.java
+++ app/src/main/java/fr/camillebriand/punkappchien/BeerDialog.java
@@ -2,17 +2,51 @@ package fr.camillebriand.punkappchien;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import android.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BeerDialog extends DialogFragment {
+	
+	private TextView beerName;
+	private ImageView beerImage;
+	private TextView beerDescription;
+	
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		LayoutInflater inflater = getActivity().getLayoutInflater();
 		
-		builder.setView(R.layout.beer_dialog);
+		View dialogView = inflater.inflate(R.layout.beer_dialog, null);
 		
+		this.beerName = dialogView.findViewById(R.id.beer_dialog__name);
+		this.beerImage = dialogView.findViewById(R.id.beer_dialog__image);
+		this.beerDescription = dialogView.findViewById(R.id.beer_dialog__description);
+		
+		builder.setView(dialogView);
 		return builder.create();
+	}
+	
+	public void setBeerName(String beerName) {
+		if (this.beerName == null) return;
+		
+		this.beerName.setText(beerName);
+	}
+	
+	public void setBeerImage(Bitmap bmp) {
+		if (this.beerImage == null) return;
+		
+		this.beerImage.setImageBitmap(bmp);
+	}
+	
+	public void setBeerDescription(String beerDescription) {
+		if (this.beerDescription == null) return;
+		
+		this.beerDescription.setText(beerDescription);
 	}
 }
