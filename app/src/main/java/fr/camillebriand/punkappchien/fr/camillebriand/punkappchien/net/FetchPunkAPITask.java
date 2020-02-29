92438436763c1a9f3b52095124fb7ab2d4ae2cff
Camille Briand <camille.briand@efrei.net>
2020-02-29 15:46:57 +0100

    Add fr.camillebriand.punkappchien.net.FetchPunkAPITask class

---
+++ app/src/main/java/fr/camillebriand/punkappchien/fr/camillebriand/punkappchien/net/FetchPunkAPITask.java
@@ -0,0 +1,10 @@
+package fr.camillebriand.punkappchien.fr.camillebriand.punkappchien.net;
+
+import android.os.AsyncTask;
+
+public class FetchPunkAPITask extends AsyncTask {
+	@Override
+	protected Object doInBackground(Object[] objects) {
+		return null;
+	}
+}
