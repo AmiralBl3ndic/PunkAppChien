c7a7a5c3ff5dfa311c27b80ff07904236c4eac98
AmiralBl3ndic <camille.briand@efrei.net>
2020-03-14 14:24:13 +0100

    Make app handle beers without images

--- app/src/main/java/fr/camillebriand/punkappchien/net/FetchPunkAPITask.java
+++ app/src/main/java/fr/camillebriand/punkappchien/net/FetchPunkAPITask.java
@@ -66,10 +66,12 @@ public class FetchPunkAPITask extends AsyncTask<Void, Void, Beer> {
 				}
 				
 				jsonApiResponse = apiResponse.getJSONObject(0);
-				imageInputStream = new URL(jsonApiResponse.getString("image_url")).openStream();
-
-				jsonApiResponse.put("image", BitmapFactory.decodeStream(imageInputStream));
-
+				
+				if (jsonApiResponse.has("image_url") && !jsonApiResponse.getString("image_url").equals("null")) {
+					imageInputStream = new URL(jsonApiResponse.getString("image_url")).openStream();
+					jsonApiResponse.put("image", BitmapFactory.decodeStream(imageInputStream));
+				}
+				
 				try {
 					beer = new Beer(jsonApiResponse, this.activityRef.get().getApplicationContext());
 				} catch (NullPointerException e) {  // Can only be thrown if activityRef.get() is null
@@ -124,7 +126,7 @@ public class FetchPunkAPITask extends AsyncTask<Void, Void, Beer> {
 			return;
 		}
 
-		Beer.addBeerToCollection(beer);
+		Beer.addBeerToFavourites(beer);
 		beerDialog.setBeer(beer);
 	}
 }
