8a0379dad724fafc6e2683aa0e3c3fb144461b1e
AmiralBl3ndic <camille.briand@efrei.net>
2020-03-14 18:51:46 +0100

    Make BeerDetailsActivity gather beer from intent

--- app/src/main/java/fr/camillebriand/punkappchien/BeerDetailsActivity.java
+++ app/src/main/java/fr/camillebriand/punkappchien/BeerDetailsActivity.java
@@ -2,7 +2,10 @@ package fr.camillebriand.punkappchien;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import fr.camillebriand.punkappchien.model.Beer;
 
 public class BeerDetailsActivity extends AppCompatActivity {
 	
@@ -10,5 +13,7 @@ public class BeerDetailsActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_beer_details);
+		
+		Beer beer = (Beer) getIntent().getSerializableExtra("beer");
 	}
 }
