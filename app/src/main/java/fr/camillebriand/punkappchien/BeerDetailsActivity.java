25c0cdc2d1489d5e9c151b4c4549369ba1148bca
AmiralBl3ndic <camille.briand@efrei.net>
2020-03-14 18:22:55 +0100

    Add BeerDetailsActivity

---
+++ app/src/main/java/fr/camillebriand/punkappchien/BeerDetailsActivity.java
@@ -0,0 +1,14 @@
+package fr.camillebriand.punkappchien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BeerDetailsActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_beer_details);
+	}
+}
