7f0408e59be242bc9c1b1b9907d5dc6fd9571496
AmiralBl3ndic <camille.briand@efrei.net>
2020-03-15 17:10:02 +0100

    Implement LoadBeersFromDatabaseTask.doInBackground method

--- app/src/main/java/fr/camillebriand/punkappchien/async/LoadBeersFromDatabaseTask.java
+++ app/src/main/java/fr/camillebriand/punkappchien/async/LoadBeersFromDatabaseTask.java
@@ -1,17 +1,30 @@
 package fr.camillebriand.punkappchien.async;
 
 import android.content.Context;
+import android.os.AsyncTask;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
 
 import fr.camillebriand.punkappchien.model.Beer;
+import fr.camillebriand.punkappchien.persistence.BeerDatabase;
 
-public class LoadBeersFromDatabaseTask extends BeerDatabaseTask {
-	protected LoadBeersFromDatabaseTask(Context context) {
-		super(context);
+public class LoadBeersFromDatabaseTask extends AsyncTask<Void, Void, Void> {
+	private WeakReference<Context> contextRef;
+	
+	public LoadBeersFromDatabaseTask(Context context) {
+		this.contextRef = new WeakReference<>(context);
 	}
 	
 	@Override
-	protected Void doInBackground(Beer... beers) {
+	protected Void doInBackground(Void... voids) {
+		if (contextRef == null || contextRef.get() == null) return null;
 		
+		List<Beer> beers = BeerDatabase.getInstance(contextRef.get()).beerDAO().getBeers();
+		
+		for (Beer beer : beers) {
+			Beer.addBeerToFavourites(beer);
+		}
 		
 		return null;
 	}
