987a70192777dd509b910404c7651afc4308a967
AmiralBl3ndic <camille.briand@efrei.net>
2020-03-15 17:14:03 +0100

    Make application read local beers database at startup

--- app/src/main/java/fr/camillebriand/punkappchien/PunkAppChien.java
+++ app/src/main/java/fr/camillebriand/punkappchien/PunkAppChien.java
@@ -2,5 +2,14 @@ package fr.camillebriand.punkappchien;
 
 import android.app.Application;
 
+import fr.camillebriand.punkappchien.async.LoadBeersFromDatabaseTask;
+
 public class PunkAppChien extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		
+		// Load beers from local database into memory
+		new LoadBeersFromDatabaseTask(getApplicationContext()).execute();
+	}
 }
